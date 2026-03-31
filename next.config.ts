eb6a31aa42191a003c74c320d628b3d1d70429e6
lcp <961943060@qq.com>
2026-03-31 14:53:19 +0800

    prevent external package

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // no external packages
+  experimental: {
+    serverComponentsExternalPackages: [],
+  },
+  // Edge running
+  unstable_allowSuspendedClientComponents: false,
 };
 
 export default nextConfig;
